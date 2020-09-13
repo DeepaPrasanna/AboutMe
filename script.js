fb36cfcc5c18e9fe33538fd6b194a8f6d749aca7
DeepaPrasanna <anneraj73@gmail.com>
2020-09-13 12:53:47 +0530

    added script

---
+++ script.js
@@ -0,0 +1,19 @@
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+name.addEventListener("input", function (event) {
+  if (name.validity.valid) {
+    name.setCustomValidity("Pls enter a valid username");
+  } else {
+    name.setCustomValidity("");
+  }
+});
